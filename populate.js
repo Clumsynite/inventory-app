20857f2852ac02ee302a64b0982925b4fec88dc1
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-29 23:24:29 +0530

    mistake rectified

--- populate.js
+++ populate.js
@@ -19,7 +19,22 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const items = [];
 const brands = [];
 
-function itemCreate(name, brand, inStock, price, added, cb) {
+const brandCreate = (name, cb) => {
+  const brand = new Brand({ name: name });
+
+  brand.save(function (err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log("New Brand: " + brand);
+    brands.push(brand);
+    cb(null, brand);
+  });
+}
+
+
+const itemCreate = (name, brand, inStock, price, added, cb) => {
   itemDetail = {
     name: name,
     brand: brand,
@@ -38,20 +53,6 @@ function itemCreate(name, brand, inStock, price, added, cb) {
     console.log("New Items: " + item);
     items.push(item);
     cb(null, item);
-  });
-}
-
-function brandCreate(name, cb) {
-  const brand = new Brand({ name: name });
-
-  brand.save(function (err) {
-    if (err) {
-      cb(err, null);
-      return;
-    }
-    console.log("New Brand: " + brand);
-    brands.push(brand);
-    cb(null, brand);
   });
 }
 
@@ -94,7 +95,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone XR (128GB) - Black",
-          brand[0],
+          brands[0],
           10,
           53500,
           new Date(),
@@ -104,7 +105,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone XR (64GB) - Coral",
-          brand[0],
+          brands[0],
           15,
           47500,
           new Date(),
@@ -114,7 +115,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 7 (32GB) - Black",
-          brand[0],
+          brands[0],
           20,
           27799,
           new Date(),
@@ -124,7 +125,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 8 (12G8B) - Gold",
-          brand[0],
+          brands[0],
           27,
           39999,
           new Date(),
@@ -134,7 +135,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 11 Pro Max (64GB) - Space Grey",
-          brand[0],
+          brands[0],
           8,
           99990,
           new Date(),
@@ -144,7 +145,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 11 Pro (64GB) - Space Grey",
-          brand[0],
+          brands[0],
           12,
           96990,
           new Date(),
@@ -154,7 +155,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 11 (256GB) - White",
-          brand[0],
+          brands[0],
           4,
           75900,
           new Date(),
@@ -164,7 +165,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 11 (512GB) - Space Grey",
-          brand[0],
+          brands[0],
           2,
           150800,
           new Date(),
@@ -174,7 +175,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "iPhone 8 Plus (256GB) - Space Grey",
-          brand[0],
+          brands[0],
           5,
           84900,
           new Date(),
@@ -186,7 +187,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M21 (Midnight Blue, 4GB RAM, 64GB Storage)",
-          brand[1],
+          brands[1],
           40,
           13999,
           new Date(),
@@ -196,7 +197,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M31 (Ocean Blue, 6GB RAM, 128GB Storage)",
-          brand[1],
+          brands[1],
           45,
           17499,
           new Date(),
@@ -206,7 +207,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M31s (Mirage Blue, 6GB RAM, 64GB Storage)",
-          brand[1],
+          brands[1],
           26,
           19499,
           new Date(),
@@ -216,7 +217,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M51 (Electric Blue, 6GB RAM, 128GB Storage)",
-          brand[1],
+          brands[1],
           40,
           24999,
           new Date(),
@@ -226,7 +227,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M01 (Black, 3GB RAM, 32GB Storage)",
-          brand[1],
+          brands[1],
           40,
           7999,
           new Date(),
@@ -236,7 +237,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M11 (Metallic Blue, 4GB RAM, 64GB Storage)",
-          brand[1],
+          brands[1],
           20,
           11999,
           new Date(),
@@ -246,7 +247,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy M01s (Blue, 3GB RAM, 32GB Storage)",
-          brand[1],
+          brands[1],
           40,
           9499,
           new Date(),
@@ -256,7 +257,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy A21s (Blue, 4GB RAM, 64GB Storage)",
-          brand[1],
+          brands[1],
           30,
           14999,
           new Date(),
@@ -266,7 +267,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy A71 (Prism Crush, 8GB RAM, 128GB Storage)",
-          brand[1],
+          brands[1],
           40,
           29999,
           new Date(),
@@ -276,7 +277,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy Note10 lite (Aura Glow, 8GB RAM, 128GB Storage)",
-          brand[1],
+          brands[1],
           40,
           39999,
           new Date(),
@@ -286,7 +287,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy Z Fold2 5G (Mystic Bronze, 12GB RAM, 25GB Storage)",
-          brand[1],
+          brands[1],
           8,
           14999,
           new Date(),
@@ -296,7 +297,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy S10 Plus (Black, 8GB RAM, 128GB Storage)",
-          brand[1],
+          brands[1],
           30,
           52999,
           new Date(),
@@ -306,7 +307,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Galaxy Note 20 Ultra (Mystic Bronze, 12GB RAM, 256GB Storage)",
-          brand[1],
+          brands[1],
           10,
           104999,
           new Date(),
@@ -318,7 +319,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus Nord 5G (Gray Onyx, 12GB RAM, 256GB Storage)",
-          brand[2],
+          brands[2],
           39,
           29999,
           new Date(),
@@ -328,7 +329,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus 7T (Glacier Blue, 6GB RAM, 128 Storage)",
-          brand[2],
+          brands[2],
           29,
           41999,
           new Date(),
@@ -338,7 +339,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus 7T (Haze Blue, 8GB RAM, 256GB Storage)",
-          brand[2],
+          brands[2],
           35,
           43999,
           new Date(),
@@ -348,7 +349,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus 8 Pro (Glacier Green, 8GB RAM, 128GB Storage)",
-          brand[2],
+          brands[2],
           19,
           54999,
           new Date(),
@@ -358,7 +359,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus 5T (Midnight Black, 6GB RAM, 64GB Storage)",
-          brand[2],
+          brands[2],
           25,
           26999,
           new Date(),
@@ -368,7 +369,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "OnePlus 6 (Mirror Black, 6GB RAM, 64GB Storage)",
-          brand[2],
+          brands[2],
           39,
           20495,
           new Date(),
@@ -380,7 +381,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 5.3 (Charcoal, 4GB RAM, 64GB Storage)",
-          brand[3],
+          brands[3],
           30,
           13999,
           new Date(),
@@ -390,7 +391,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 2.3 (Charcoal, 2GB RAM, 32GB Storage)",
-          brand[3],
+          brands[3],
           40,
           8650,
           new Date(),
@@ -400,7 +401,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia C3 (Black, 2GB RAM, 16GB Storage)",
-          brand[3],
+          brands[3],
           23,
           7499,
           new Date(),
@@ -410,7 +411,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 4.2 (Black, 3GB RAM, 32GB Storage)",
-          brand[3],
+          brands[3],
           25,
           11999,
           new Date(),
@@ -420,7 +421,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 3.2 (Black, 3GB RAM, 32GB Storage)",
-          brand[3],
+          brands[3],
           35,
           10690,
           new Date(),
@@ -430,7 +431,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 7.2 (Cyan Green, 4GB RAM, 64GB Storage)",
-          brand[3],
+          brands[3],
           32,
           19999,
           new Date(),
@@ -440,7 +441,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Nokia 9 PureView (Blue, 6GB RAM, 128GB Storage)",
-          brand[3],
+          brands[3],
           20,
           34875,
           new Date(),
@@ -452,7 +453,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "9 (Carbon, 4GB RAM, 64GB Storage)",
-          brand[4],
+          brands[4],
           30,
           8999,
           new Date(),
@@ -462,7 +463,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "8A Dual (Sea Blue, 2GB RAM, 32GB Storage)",
-          brand[4],
+          brands[4],
           30,
           7499,
           new Date(),
@@ -472,7 +473,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "K20 Pro (Flame Red, 6GB RAM, 128GB Storage)",
-          brand[4],
+          brands[4],
           10,
           23501,
           new Date(),
@@ -484,7 +485,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "G Plus, 4th Gen (White, 32GB Storage)",
-          brand[5],
+          brands[5],
           30,
           9800,
           new Date(),
@@ -494,7 +495,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Raz Gold (Gold, 6GB RAM)",
-          brand[5],
+          brands[5],
           3,
           114400,
           new Date(),
@@ -504,7 +505,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Edge+ (Thunder Grey, 12GB RAM, 256GB Storage)",
-          brand[5],
+          brands[5],
           10,
           72490,
           new Date(),
@@ -514,7 +515,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Razr (Black, 6GB RAM, 128GB Storage)",
-          brand[5],
+          brands[5],
           30,
           7499,
           new Date(),
@@ -526,7 +527,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Zenfone Max M2 (Black, 3GB RAM, 32GB Storage)",
-          brand[6],
+          brands[6],
           20,
           7999,
           new Date(),
@@ -536,7 +537,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Zenfone 3 (Gold, 3GB RAM, 32GB Storage)",
-          brand[6],
+          brands[6],
           21,
           13999,
           new Date(),
@@ -546,7 +547,7 @@ function createItems(cb) {
       (callback) => {
         itemCreate(
           "Zenfone Max M1 (Gold, 3GB RAM, 32GB Storage)",
-          brand[6],
+          brands[6],
           12,
           6999,
           new Date(),
