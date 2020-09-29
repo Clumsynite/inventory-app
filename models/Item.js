0db32bf212ecd6b85d76fa40282d7ea6e3b4b68f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-29 23:24:13 +0530

    Added virtuals to get price with comma and item name without specs

--- models/Item.js
+++ models/Item.js
@@ -14,4 +14,14 @@ ItemSchema.virtual("url").get(function () {
   return "item/" + this._id;
 });
 
+ItemSchema.virtual("price_formatted").get(function () {
+  return new Intl.NumberFormat("en-IN", { maximumSignificantDigits: 3 }).format(
+    this.price
+  );
+});
+
+ItemSchema.virtual("item_name").get(function () {
+  return this.name.substring(0, this.name.indexOf('('))
+});
+
 module.exports = mongoose.model("Item", ItemSchema);
