b981ae698cf5f009f6c537968c9141798347906a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-30 23:20:04 +0530

    Added get and post control for update

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -64,11 +64,49 @@ exports.item_new_post = (req, res, next) => {
     brand: req.body.brand,
     price: req.body.price,
     inStock: req.body.quantity,
-    added: new Date()
+    added: new Date(),
   });
 
   item.save((err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect(item.url);
+  });
+};
+
+exports.item_update_get = (req, res, next) => {
+  async.parallel({
+    item: (callback) => {
+      Item.findById(req.params.id).populate("brand").exec(callback);
+    },
+    brands: callback => {
+      Brand.find({}).exec(callback)
+    }
+  }, (err, data) => {
     if (err) { return next(err) }
-    res.redirect(item.url)
+    if(data.item==null) {
+      const err = new Error('Item not found')
+      err.status = 404
+      return next(err)
+    }
+    res.render('item_form', {title: `Update ${data.item.name}`, item: data.item, brands: data.brands})
+
+  });
+};
+
+exports.item_update_post = (req, res, next) => {
+
+  const item = new Item({
+    name: req.body.name,
+    brand: req.body.brand,
+    price: req.body.price,
+    inStock: req.body.quantity,
+    added: new Date(),
+    _id: req.params.id
+  })
+  Item.findByIdAndUpdate(req.params.id, item, {}, (err, data) => {
+    if (err) { return next(err); }
+    res.redirect(data.url);
   })
 };
