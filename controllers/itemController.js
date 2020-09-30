db6733f8db11f0251a16172688e15ea8dfc18b22
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-30 12:17:55 +0530

    refactored using prettier

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,27 +1,31 @@
-const Item = require('../models/Item');
-const Brand = require('../models/brand');
+const Item = require("../models/Item");
+const Brand = require("../models/brand");
 
-const async = require('async');
+const async = require("async");
 
 exports.index = (req, res, next) => {
-  async.parallel({
-    items: (callback) => {
-      Item.countDocuments({}, callback)
+  async.parallel(
+    {
+      items: (callback) => {
+        Item.countDocuments({}, callback);
+      },
+      brand: (callback) => {
+        Brand.countDocuments({}, callback);
+      },
     },
-    brand: (callback) => {
-      Brand.countDocuments({}, callback)
+    (err, results) => {
+      res.render("index", { title: "Inventory", data: results, error: err });
     }
-  }, (err, results) => {
-    res.render('index', {title: 'Inventory', data: results, error: err})
-    console.log(results);
-  })
-}
+  );
+};
 
 exports.item_list = (req, res, next) => {
   Item.find({})
-  .populate('brand')
-  .exec((err, items) => {
-    if (err) { return next(err); }
-    res.render('item_list', { title: 'Items', item_list: items });
-  })
-}
+    .populate("brand")
+    .exec((err, items) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("item_list", { title: "Items", item_list: items });
+    });
+};
