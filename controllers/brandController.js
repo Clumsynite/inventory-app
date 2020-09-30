315aff9c29a67727377a8a0b7ea35dda78e6f7b2
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-30 14:20:43 +0530

    Added controller to get brand view on click

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -4,5 +4,27 @@ const Brand = require("../models/brand");
 const async = require("async");
 
 exports.brand_view = (req, res, next) => {
-  
-}
+  async.parallel(
+    {
+      brand: (callback) => {
+        Brand.findById(req.params.id).exec(callback);
+      },
+      brand_items: (callback) => {
+        Item.find({ brand: req.params.id }).populate("brand").exec(callback);
+      },
+      brands: (callback) => {
+        Brand.find({}, callback);
+      },
+    },
+    (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("brand_view", {
+        title: data.brand.name,
+        item_list: data.brand_items,
+        brands: data.brands,
+      });
+    }
+  );
+};
