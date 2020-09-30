f1711017dd445c2fa6122bd15ba377b1f3af261a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-30 22:05:59 +0530

    Added routing to update items and brands

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,18 @@ router.get('/item/new', itemController.item_new_get)
 
 router.post('/item/new', itemController.item_new_post)
 
+router.get('/item/:id/update', itemController.item_update_get)
+
+router.post('/item/:id/update', itemController.item_update_post)
+
 router.get('/brand/:id/view', brandController.brand_view)
 
 router.get('/brand/new', brandController.brand_new_get)
 
 router.post('/brand/new', brandController.brand_new_post)
+
+router.get('/brand/:id/update', brandController.brand_update_get)
+
+router.post('/brand/:id/update', brandController.brand_update_post)
 
 module.exports = router;
