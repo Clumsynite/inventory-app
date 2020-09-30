434b16f9dfcd3abae3ef8941ca98da94cc3e6928
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-30 13:11:55 +0530

    removed /brand routing

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,12 +3,6 @@ const Brand = require("../models/brand");
 
 const async = require("async");
 
-exports.brand_list = (req, res, next) => {
-  Brand.find({})
-    .exec((err, items) => {
-      if (err) {
-        return next(err);
-      }
-      res.render("brand_list", { title: "Brands", brands: items });
-    });
-};
+exports.brand_view = (req, res, next) => {
+  
+}
