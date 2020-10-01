1b5cdd3bc3b35e4b7d0e2ad04fa86b80aca6b064
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-01 13:27:52 +0530

    removed helmet library

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 
 const compression = require('compression');
-const helmet = require('helmet');
-
 const app = express();
 
 // view engine setup
@@ -21,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression())
-app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
