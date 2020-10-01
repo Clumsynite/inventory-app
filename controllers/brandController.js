d624911293790e80f59694fc4427f36262a9ac3c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-01 12:13:58 +0530

    Added brand delete page

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -71,18 +71,75 @@ exports.brand_update_get = (req, res, next) => {
 exports.brand_update_post = (req, res, next) => {
   const brand = new Brand({
     name: req.body.name,
-    _id: req.params.id
-  }) 
+    _id: req.params.id,
+  });
   Brand.findByIdAndUpdate(req.params.id, brand, {}, (err, data) => {
-    if (err) {return next(err)}
-    res.redirect(brand.url)
-  })
+    if (err) {
+      return next(err);
+    }
+    res.redirect(brand.url);
+  });
 };
 
 exports.brand_delete_get = (req, res, next) => {
-
-}
+  async.parallel(
+    {
+      items: (callback) => {
+        Item.find({ brand: req.params.id }).exec(callback);
+      },
+      brands: (callback) => {
+        Brand.find({}, callback);
+      },
+      brand: (callback) => {
+        Brand.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      if (data.items === null) {
+        res.redirect("/");
+      }
+      res.render("brand_delete", {
+        title: `Delete ${data.brand.name}`,
+        brand: data.brand,
+        items: data.items,
+        brands: data.brands,
+      });
+    }
+  );
+};
 
 exports.brand_delete_post = (req, res, next) => {
-  
-}
+  async.parallel(
+    {
+      items: (callback) => {
+        Item.find({ brand: req.params.id }).exec(callback);
+      },
+      brand: (callback) => {
+        Brand.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      if (data.items.length > 0) {
+        res.render("item_delete", {
+          title: "Delete Item",
+          brand: results.brand,
+          item: results.item,
+        });
+        return;
+      } else {
+        Brand.findByIdAndRemove(req.body.brandid, (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/");
+        });
+      }
+    }
+  );
+};
